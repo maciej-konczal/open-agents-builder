6952a22878905d66626ac317a1ac78f7d0e89542
Piotr Karwatka <piotr.karwatka@gmail.com>
2025-01-28 20:16:45 +0100

    [feat] template context

---
+++ src/contexts/template-context.tsx
@@ -0,0 +1,122 @@
+import { ApiEncryptionConfig } from '@/data/client/admin-api-client';
+import { Agent, DataLoadingStatus, Session, Result } from '@/data/client/models';
+import { AgentDTO, PaginatedQuery, PaginatedResult } from '@/data/dto';
+import React, { createContext, useState, useContext, ReactNode } from 'react';
+import { DatabaseContext } from './db-context';
+import { AgentApiClient, DeleteAgentResponse } from '@/data/client/agent-api-client';
+import { SaaSContext } from './saas-context';
+import { toast } from 'sonner';
+import { useTranslation } from 'react-i18next';
+import { nanoid } from 'nanoid';
+import { AuditContext } from './audit-context';
+import { diff, addedDiff, deletedDiff, updatedDiff, detailedDiff } from 'deep-object-diff';
+import { TemplateApiClient } from '@/data/client/template-api-client';
+
+interface TemplateContextType {
+    templates: Agent[];
+    updateTemplate: (agent: Agent) => Promise<Agent>;
+    listTemplates: () => Promise<Agent[]>;
+    setTemplates: (templates: Agent[]) => void;
+    deleteTemplate: (agent: Agent) => Promise<DeleteAgentResponse>;
+
+}
+
+const TemplateContext = createContext<TemplateContextType | undefined>(undefined);
+
+export const TemplateProvider = ({ children }: { children: ReactNode }) => {
+    const [templates, setTemplates] = useState<Agent[]>([]);
+    const auditContext = useContext(AuditContext);
+
+    const { t } = useTranslation();
+
+    const dbContext = useContext(DatabaseContext);
+    const saasContext = useContext(SaaSContext);
+
+
+    const setupApiClient = async () => {
+        const encryptionConfig: ApiEncryptionConfig = {
+            secretKey: dbContext?.masterKey,
+            useEncryption: false // TODO: add a env variable config for this
+        };
+        const client = new TemplateApiClient('', dbContext, saasContext, encryptionConfig);
+        return client;
+    }        
+
+    const updateTemplate = async (agent:Agent): Promise<Agent> => {
+
+        const client = await setupApiClient();
+        const agentDTO = agent.toDTO(); // DTOs are common ground between client and server
+        const newRecord = typeof agent?.id  === 'undefined' || agent.id === 'new';
+
+        const prevRecord = templates.find(r => r.id === agent.id);
+        const changes = prevRecord ?  detailedDiff(prevRecord, agent) : {};
+        
+        if (newRecord) agentDTO.id = nanoid();
+        const response = await client.put(agentDTO);
+
+        if (response.status !== 200) {
+            console.error('Error adding template:', response.message);
+            toast.error(t('Error adding template'));
+
+            return agent;
+        } else {
+            const updatedAgent = Object.assign(agent, { id: response.data.id });
+            setTemplates(
+                newRecord ? [...templates, updatedAgent] :
+                templates.map(pr => pr.id === agent.id ?  agent : pr)
+            )
+            auditContext?.record({ eventName: newRecord ? 'addTemplate' : 'updateTemplate', recordLocator: JSON.stringify({ id: agent.id }), encryptedDiff: JSON.stringify(changes) })
+            return updatedAgent;
+        }
+
+    };
+
+    const deleteTemplate = async (agent: Agent): Promise<DeleteAgentResponse> => {
+        const client = await setupApiClient();
+        const response = await client.delete(agent.toDTO());
+        if (response.status !== 200) {
+            console.error('Error deleting agent:', response.message);
+            toast.error(t('Error deleting agent'));
+        } else {
+            auditContext?.record({ eventName: 'deleteAgent', recordLocator: agent.id  })
+
+            setTemplates(templates.filter(pr => pr.id !== agent.id));
+            toast.success(t('Template deleted'));
+        }
+
+        return response;
+    }
+
+    const listTemplates = async (): Promise<Agent[]> => {
+        const client = await setupApiClient();
+        try {
+            const apiResponse = await client.get();
+            const fetchedAgents = apiResponse.map((folderDTO: AgentDTO) => Agent.fromDTO(folderDTO));
+            setTemplates(fetchedAgents);
+            return fetchedAgents;
+        } catch(error) {
+            throw (error)
+        }; 
+
+
+    }
+    return (
+        <TemplateContext.Provider value={{ 
+            templates,
+            listTemplates,
+            deleteTemplate,
+            updateTemplate,
+            setTemplates
+            }}>
+            {children}
+        </TemplateContext.Provider>
+    );
+};
+
+export const useTemplateContext = (): TemplateContextType => {
+    const context = useContext(TemplateContext);
+    if (context === undefined) {
+        throw new Error('useAgentContext must be used within an AgentProvider');
+    }
+    return context;
+};
---
+++ src/data/client/template-api-client.ts
@@ -0,0 +1,27 @@
+import { AdminApiClient, ApiEncryptionConfig } from "./admin-api-client";
+import { SaaSContextType } from "@/contexts/saas-context";
+import { AgentDTO, AgentDTOEncSettings } from "../dto";
+import { DatabaseContextType } from "@/contexts/db-context";
+import { DeleteAgentResponse, GetAgentsResponse, PutAgentRequest, PutAgentResponse } from "./agent-api-client";
+
+
+export class TemplateApiClient extends AdminApiClient {
+    constructor(baseUrl: string, dbContext?: DatabaseContextType | null, saasContext?: SaaSContextType | null, encryptionConfig?: ApiEncryptionConfig) {
+      super(baseUrl, dbContext, saasContext, encryptionConfig);
+    }
+  
+    async get(agentId?:string): Promise<GetAgentsResponse> {
+      if (agentId)
+        return this.request<GetAgentsResponse>('/api/agent/template?id=' + encodeURIComponent(agentId) , 'GET', AgentDTOEncSettings) as Promise<GetAgentsResponse>;
+    else 
+        return this.request<GetAgentsResponse>('/api/agent/template' , 'GET', AgentDTOEncSettings) as Promise<GetAgentsResponse>;
+    }
+
+    async put(record: PutAgentRequest): Promise<PutAgentResponse> {
+      return this.request<PutAgentResponse>('/api/agent/template/', 'PUT', AgentDTOEncSettings, record) as Promise<PutAgentResponse>;
+    }
+
+    async delete(record: AgentDTO): Promise<DeleteAgentResponse> {
+      return this.request<DeleteAgentResponse>('/api/agent/template/' + record.id, 'DELETE', { ecnryptedFields: [] }) as Promise<DeleteAgentResponse>;
+    }    
+}
