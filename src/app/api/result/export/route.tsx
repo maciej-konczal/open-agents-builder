01e572066f0aa936cc10da4ca99152c24ba13512
pkarw <piotr@catchthetornado.com>
2025-02-04 13:43:00 +0700

    [feat] result export WIP

--- src/app/api/result/export/route.tsx
+++ src/app/api/result/export/route.tsx
@@ -2,16 +2,52 @@ import { ResultDTO } from "@/data/dto";
 import ServerResultRepository from "@/data/server/server-result-repository";
 import { authorizeRequestContext, genericGET } from "@/lib/generic-api";
 import { NextRequest, NextResponse } from "next/server";
+import JSZip, { file } from 'jszip'
+import showdown from "showdown";
+import filenamify from 'filenamify';
+import * as json2html from 'json2html';
 
 
 export async function GET(request: NextRequest, response: NextResponse) {
     const requestContext = await authorizeRequestContext(request, response);
     const resultRepo = new ServerResultRepository(requestContext.databaseIdHash);
-    const allResults = resultRepo.findAll();
+    const allResults = await resultRepo.findAll();
 
     const zip = new JSZip();
     const converter = new showdown.Converter({ tables: true, completeHTMLDocument: true, openLinksInNewWindow: true });
     converter.setFlavor('github');
 
-    return Response.json(await genericGET<ResultDTO>(request, new ServerResultRepository(requestContext.databaseIdHash)));
+    let indexMd = '# Agent Doodle Export\n\n';
+
+    for (const result of allResults) {
+        if (result.content) {
+            const recordNiceName = filenamify((result.createdAt + ' - ' + result.sessionId), {replacement: '-'});
+            indexMd += ` - <a href="${recordNiceName}.md">${result.createdAt} - ${result.sessionId}</a>\n\n`;
+
+            let html = ''
+            if (result.format?.toLowerCase() === 'markdown') {
+                html = converter.makeHtml(result.content);
+                zip.file(`${recordNiceName}.md`, result.content);
+            } 
+
+            if (result.format?.toLowerCase() === 'json') {
+                html = json2html.render(result.content);
+                zip.file(`${recordNiceName}.json`, JSON.stringify(result.content));
+            }
+                
+            zip.file(`${recordNiceName}.html`, html);
+        }
+    }
+    zip.file('index.md', indexMd);
+    zip.file('index.html', converter.makeHtml(indexMd.replaceAll('.md', '.html')));
+    zip.file('results.json', JSON.stringify(allResults));
+
+    const headers = new Headers();
+
+    headers.set("Content-Type", "application/zip");
+    const zipFileContent = await zip.generateAsync({type:"blob"});
+
+    // or just use new Response ❗️
+    return new NextResponse(zipFileContent, { status: 200, statusText: "OK", headers });
 }
+
