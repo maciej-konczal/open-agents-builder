0879e370a5f34c360559f9598d2f69946273312e
pkarw <piotr@catchthetornado.com>
2025-02-06 16:34:01 +0700

    [fix] unused imports removed

--- src/app/[locale]/agent/[id]/sessions/[sessionId]/page.tsx
+++ src/app/[locale]/agent/[id]/sessions/[sessionId]/page.tsx
@@ -6,20 +6,12 @@ import { getErrorMessage } from '@/lib/utils';
 import { toast } from 'sonner';
 import { useParams } from 'next/navigation';
 import { Card, CardContent } from '@/components/ui/card';
-import { Result, Session } from '@/data/client/models';
-import { RenderResult } from '@/components/render-result';
-import { Tabs } from '@/components/ui/tabs';
-import { TabsContent, TabsList, TabsTrigger } from '@radix-ui/react-tabs';
+import { Session } from '@/data/client/models';
 import { ChatMessages, DisplayToolResultsMode } from '@/components/chat-messages';
-import ResultDetails from '@/components/result-details';
 import { Button } from '@/components/ui/button';
-import { CopyIcon, MessageCircleIcon, MoveLeftIcon, SaveIcon, WandSparkles } from 'lucide-react';
+import { MoveLeftIcon } from 'lucide-react';
 import { useCopyToClipboard } from 'react-use';
 import { DatabaseContext } from '@/contexts/db-context';
-import { useChat } from 'ai/react';
-import { nanoid } from 'nanoid';
-import { Chat } from '@/components/chat';
-import { Credenza, CredenzaTrigger, CredenzaContent } from '@/components/credenza';
 import { RenderSession } from '@/components/render-session';
 
 
