e576c6e3fa3b86b800874e2664d181a6e90c79cd
pkarw <piotr@catchthetornado.com>
2025-02-13 11:46:35 +0700

    [fix] middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,32 +4,35 @@ import { i18nRouter } from 'next-i18n-router';
 import i18nConfig from '@/app/i18nConfig';
 
 export async function middleware(request: NextRequest) {
-    return i18nRouter(request, i18nConfig);
+    if (request.url.indexOf('/api/') > 0) {
+        const authorizationHeader = request.headers.get('Authorization');
+        const jwtToken = authorizationHeader?.replace('Bearer ', '');
 
-    const authorizationHeader = request.headers.get('Authorization');
-    const jwtToken = authorizationHeader?.replace('Bearer ', '');
+        if (!jwtToken) {
+            return NextResponse.json({ message: 'Unauthorized', status: 401 }, { status: 401 });
+        } else {
+            try {
+                const decoded = await jwtVerify(jwtToken, new TextEncoder().encode(process.env.NEXT_PUBLIC_TOKEN_SECRET || 'Jeipho7ahchue4ahhohsoo3jahmui6Ap'));
+                const checkDbHeader = request.headers.get('database-id-hash') === decoded.payload.databaseIdHash;
 
-    if (!jwtToken) {
-        return NextResponse.json({ message: 'Unauthorized', status: 401 }, { status: 401 });
-    } else {
-        try {
-            const decoded = await jwtVerify(jwtToken, new TextEncoder().encode(process.env.NEXT_PUBLIC_TOKEN_SECRET || 'Jeipho7ahchue4ahhohsoo3jahmui6Ap'));
-            const checkDbHeader = request.headers.get('database-id-hash') === decoded.payload.databaseIdHash;
+                if(!checkDbHeader) {
+                    return NextResponse.json({ message: 'Unauthorized', status: 401 }, { status: 401 });
+                }
 
-            if(!checkDbHeader) {
+            } catch (error) {
+                console.log(error);
                 return NextResponse.json({ message: 'Unauthorized', status: 401 }, { status: 401 });
             }
 
-        } catch (error) {
-            console.log(error);
-            return NextResponse.json({ message: 'Unauthorized', status: 401 }, { status: 401 });
         }
-
+    } else {
+        return i18nRouter(request, i18nConfig);
     }
+
 
 }
  
 export const config = {
-  matcher: '/((?!api|static|.*\\..*|_next).*)',
+  matcher: '/((?!api/db|static|.*\\..*|_next).*)',
   //matcher: ['/((?!api/db|agent|dashboard|chat|api/saas|_next/static|content|_next/image|img|onboarding|manifest|favicon.ico|$).*)'],
 }
