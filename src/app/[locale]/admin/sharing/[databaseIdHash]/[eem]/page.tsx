a5f307e9558bd7c3fa21d2d09a6848ec4db9e998
pkarw <piotr@catchthetornado.com>
2025-02-14 06:58:59 +0100

    [fix] redirection loader restored

--- src/app/[locale]/admin/sharing/[databaseIdHash]/[eem]/page.tsx
+++ src/app/[locale]/admin/sharing/[databaseIdHash]/[eem]/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import DataLoader from "@/components/data-loader";
 import { DatabaseContext } from "@/contexts/db-context";
 import { useRouter } from "next/navigation";
 import { useContext, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function SharingPage({children,
   params,
@@ -12,6 +14,7 @@ export default function SharingPage({children,
 })  {
     const router = useRouter();
 
+    const { t } = useTranslation();
     const dbContext = useContext(DatabaseContext);
 
     useEffect(() => {
@@ -25,8 +28,8 @@ export default function SharingPage({children,
     return (       
         <div className="pt-10">
           <div className="text-center">
-            {/* <div className="flex justify-center m-4"><DataLoader /></div>
-            <div className="text-gray-500 text-center">{t("Succesfully authorized, redirecting to app...")}</div> */}
+            <div className="flex justify-center m-4"><DataLoader /></div>
+            <div className="text-gray-500 text-center">{t("Succesfully authorized, redirecting to app...")}</div>
           </div>
         </div>
     )
