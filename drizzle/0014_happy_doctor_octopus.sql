80cf7cba3653cf0465b1116a6f45e129267877d1
Piotr Karwatka <piotr.karwatka@gmail.com>
2025-02-17 13:26:23 +0100

    [fix] schema fixes

--- drizzle/0014_happy_doctor_octopus.sql
+++ drizzle/0014_happy_doctor_octopus.sql
@@ -1,14 +1,16 @@
 CREATE TABLE `calendarEvents` (
 	`id` text PRIMARY KEY NOT NULL,
-	`agentId` text,
 	`title` text,
 	`start` text NOT NULL,
 	`end` text NOT NULL,
 	`exclusive` text,
 	`description` text,
 	`location` text,
+	`agentId` text,
 	`participants` text,
 	`createdAt` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	`updatedAt` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	FOREIGN KEY (`agentId`) REFERENCES `agents`(`id`) ON UPDATE no action ON DELETE no action
+	`sessionId` text,
+	FOREIGN KEY (`agentId`) REFERENCES `agents`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`sessionId`) REFERENCES `sessions`(`id`) ON UPDATE no action ON DELETE no action
 );
