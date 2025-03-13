ad82ed19b2c5e02074314c483b8d5dbdf92385fb
Piotr Karwatka <piotr.karwatka@gmail.com>
2025-03-13 13:50:12 +0100

    [feat] tool result copy to clipboard and save

--- src/components/chat-message-tool-response.tsx
+++ src/components/chat-message-tool-response.tsx
@@ -1,17 +1,21 @@
 import { useTranslation } from "react-i18next";
-import { Credenza, CredenzaContent, CredenzaTrigger } from "./credenza";
+import { Credenza, CredenzaContent, CredenzaFooter, CredenzaTrigger } from "./credenza";
 import JsonView from "@uiw/react-json-view";
 import { ScrollArea } from "./ui/scroll-area";
-import { CodeIcon } from "lucide-react";
+import { CodeIcon, CopyIcon, SaveIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { useTheme } from "next-themes";
 import { githubLightTheme } from '@uiw/react-json-view/githubLight';
 import { githubDarkTheme } from '@uiw/react-json-view/githubDark';
+import { useCopyToClipboard } from "react-use";
+import { toast } from "sonner";
 
 export function ChatMessageToolResponse( { args, result }: { args: any, result: any}) {
     const { t } = useTranslation();
     const { theme, systemTheme } = useTheme();
     const currentTheme = (theme === 'system' ? systemTheme : theme)
+
+    const [, copy] = useCopyToClipboard();
     
     return (
         <Credenza >
@@ -30,6 +34,23 @@ export function ChatMessageToolResponse( { args, result }: { args: any, result: 
                 <ScrollArea className="h-96">
                    <JsonView style={currentTheme === 'dark' ? githubDarkTheme : githubLightTheme } value={result} />
                 </ScrollArea>
+                <div className="flex">
+                    <Button size={"sm"} variant={"ghost"} onClick={() => {
+                        copy(JSON.stringify(result));
+                        toast.success(t('Result copied to clipboard'));
+                    }}><CopyIcon /></Button>
+                    <Button size={"sm"} variant={"ghost"} onClick={() => {
+                        const blob = new Blob([JSON.stringify(result, null, 2)], { type: 'application/json' });
+                        const url = URL.createObjectURL(blob);
+                        const a = document.createElement('a');
+                        a.href = url;
+                        a.download = 'result.json';
+                        a.click();
+                        URL.revokeObjectURL(url);
+                        toast.success(t('File downloaded successfully'));
+                    }
+                    }><SaveIcon /></Button>
+                </div>
             </CredenzaContent>
         </Credenza>
     )    
